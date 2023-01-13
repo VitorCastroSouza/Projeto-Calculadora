621b14faace23bb0c46c87f835bb9963057a29f1
Vitor de Castro Souza <vitor_c._souza@hotmail.com>
2023-01-13 10:40:34 -0300

    deu certo

--- calculadora/script.js
+++ calculadora/script.js
@@ -1,11 +1,11 @@
-let n1txt = document.getElementById('n1')
-let n1int = parseInt(n1txt.value)
-let n2txt = document.getElementById('n2')
-let n2int = parseInt(n2txt.value)
-let restrue = document.getElementById('res')
-let mat
 
 function soma(){
-    mat.inne = n1int.value + n2int.value
-    restrue.innerHTML = mat
+    n1txt = window.document.getElementById('n1')
+    n2txt = window.document.getElementById('n2')
+    resposta = window.document.getElementById('res')
+    n1int = parseInt(n1txt.value)
+    n2int = parseInt(n2txt.value)
+    richtroll = n1int + n2int
+    resposta.innerHTML = richtroll
+    
 }
