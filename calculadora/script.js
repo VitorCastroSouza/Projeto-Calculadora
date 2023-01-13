624c366a54a27f949a51f29f4bc8f70e10785c9f
Vitor de Castro Souza <vitor_c._souza@hotmail.com>
2023-01-13 12:36:56 -0300

    .

--- calculadora/script.js
+++ calculadora/script.js
@@ -1,8 +1,8 @@
-
+n1txt = window.document.getElementById('n1')
+n2txt = window.document.getElementById('n2')
+resposta = window.document.getElementById('res')
 function soma(){
-    n1txt = window.document.getElementById('n1')
-    n2txt = window.document.getElementById('n2')
-    resposta = window.document.getElementById('res')
+    
     n1int = parseInt(n1txt.value)
     n2int = parseInt(n2txt.value)
     richtroll = n1int + n2int
