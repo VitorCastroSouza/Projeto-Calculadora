6389e416f11604584a03226159f5f28427c3756f
Vitor de Castro Souza <vitor_c._souza@hotmail.com>
2023-01-17 16:01:51 -0300

    inicio do sucesso

--- calculadora melhor/script.js
+++ calculadora melhor/script.js
@@ -1,5 +1,35 @@
+let botao = window.document.getElementById('tela')
+let pv = 0
+let sv
+let operacao
+let resultado
+
 function mostrar(digito){
-    let botao = window.document.getElementById('tela')
-    let n = digito
-    botao.innerText = n
+    botao.value += digito
+}
+
+function limpar(){
+    botao.value = ""
+    resultado = 0
+    pv = 0
+    sv = 0
+}
+
+function soma(){
+    if(pv==0){
+        pv = botao.value
+        botao.value = ""
+} else{
+    sv = botao.value
+    n1int = parseInt(pv)
+    n2int = parseInt(sv)
+    resultado = n1int + n2int
+    pv = resultado
+    botao.value = ""
+    
+}
+}
+
+function resul(){
+    botao.value = resultado
 }
