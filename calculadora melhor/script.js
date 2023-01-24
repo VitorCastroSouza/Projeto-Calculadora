dd78514fa60dc1bdb044e5f84dd7d3939e54e51c
Vitor de Castro Souza <vitor_c._souza@hotmail.com>
2023-01-24 13:39:59 -0300

    funcionando

--- calculadora melhor/script.js
+++ calculadora melhor/script.js
@@ -32,6 +32,58 @@ function soma(){
 }
 }
 
+function subtracao(){
+    if(pv==0){
+        pv = botao.value
+        botao.value = ""
+        operacao = 2
+} else{
+    sv = botao.value
+    n1int = parseInt(pv)
+    n2int = parseInt(sv)
+    resultado = n1int - n2int
+    pv = resultado
+    botao.value = ""
+    operacao = 2
+    
+}
+}
+
+function multiplicacao(){
+    if(pv==0){
+        pv = botao.value
+        botao.value = ""
+        operacao = 3
+} else{
+    sv = botao.value
+    n1int = parseInt(pv)
+    n2int = parseInt(sv)
+    resultado = n1int * n2int
+    pv = resultado
+    botao.value = ""
+    operacao = 3
+    
+}
+}
+
+function divisao(){
+    if(pv==0){
+        pv = botao.value
+        botao.value = ""
+        operacao = 4
+} else{
+    sv = botao.value
+    n1int = parseInt(pv)
+    n2int = parseInt(sv)
+    resultado = n1int / n2int
+    pv = resultado
+    botao.value = ""
+    operacao = 4
+    
+}
+}
+
+
 function resul(){
     if(operacao==1){
         sv = 0
@@ -41,5 +93,33 @@ function resul(){
         resultado = n1int + resint
         pv = resultado
     }
+
+    if(operacao==2){
+        sv = 0
+        n1int = parseInt(pv)
+        restxt = botao.value
+        resint = parseInt(restxt)
+        resultado = n1int - resint
+        pv = resultado
+    }
+
+    if(operacao==3){
+        sv = 0
+        n1int = parseInt(pv)
+        restxt = botao.value
+        resint = parseInt(restxt)
+        resultado = n1int * resint
+        pv = resultado
+    }
+
+    if(operacao==4){
+        sv = 0
+        n1int = parseInt(pv)
+        restxt = botao.value
+        resint = parseInt(restxt)
+        resultado = n1int / resint
+        pv = resultado
+    }
+
     botao.value = resultado
 }
