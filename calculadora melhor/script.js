204196aa4083861acef7ef799e8155ab0dd6cd24
Vitor de Castro Souza <vitor_c._souza@hotmail.com>
2023-01-24 14:39:53 -0300

    resulvido

--- calculadora melhor/script.js
+++ calculadora melhor/script.js
@@ -23,11 +23,41 @@ function soma(){
         botao.value = ""
         operacao = 1
 } else{
-    sv = botao.value
-    n1int = parseInt(pv)
-    n2int = parseInt(sv)
-    resultado = n1int + n2int
-    pv = resultado
+    if(operacao==1){
+        sv = 0
+        n1int = parseInt(pv)
+        restxt = botao.value
+        resint = parseInt(restxt)
+        resultado = n1int + resint
+        pv = resultado
+    }
+
+    if(operacao==2){
+        sv = 0
+        n1int = parseInt(pv)
+        restxt = botao.value
+        resint = parseInt(restxt)
+        resultado = n1int - resint
+        pv = resultado
+    }
+
+    if(operacao==3){
+        sv = 0
+        n1int = parseInt(pv)
+        restxt = botao.value
+        resint = parseInt(restxt)
+        resultado = n1int * resint
+        pv = resultado
+    }
+
+    if(operacao==4){
+        sv = 0
+        n1int = parseInt(pv)
+        restxt = botao.value
+        resint = parseInt(restxt)
+        resultado = n1int / resint
+        pv = resultado
+    }
     botao.value = ""
     operacao = 1
     
@@ -40,11 +70,41 @@ function subtracao(){
         botao.value = ""
         operacao = 2
 } else{
-    sv = botao.value
-    n1int = parseInt(pv)
-    n2int = parseInt(sv)
-    resultado = n1int - n2int
-    pv = resultado
+    if(operacao==1){
+        sv = 0
+        n1int = parseInt(pv)
+        restxt = botao.value
+        resint = parseInt(restxt)
+        resultado = n1int + resint
+        pv = resultado
+    }
+
+    if(operacao==2){
+        sv = 0
+        n1int = parseInt(pv)
+        restxt = botao.value
+        resint = parseInt(restxt)
+        resultado = n1int - resint
+        pv = resultado
+    }
+
+    if(operacao==3){
+        sv = 0
+        n1int = parseInt(pv)
+        restxt = botao.value
+        resint = parseInt(restxt)
+        resultado = n1int * resint
+        pv = resultado
+    }
+
+    if(operacao==4){
+        sv = 0
+        n1int = parseInt(pv)
+        restxt = botao.value
+        resint = parseInt(restxt)
+        resultado = n1int / resint
+        pv = resultado
+    }
     botao.value = ""
     operacao = 2
     
@@ -57,11 +117,41 @@ function multiplicacao(){
         botao.value = ""
         operacao = 3
 } else{
-    sv = botao.value
-    n1int = parseInt(pv)
-    n2int = parseInt(sv)
-    resultado = n1int * n2int
-    pv = resultado
+    if(operacao==1){
+        sv = 0
+        n1int = parseInt(pv)
+        restxt = botao.value
+        resint = parseInt(restxt)
+        resultado = n1int + resint
+        pv = resultado
+    }
+
+    if(operacao==2){
+        sv = 0
+        n1int = parseInt(pv)
+        restxt = botao.value
+        resint = parseInt(restxt)
+        resultado = n1int - resint
+        pv = resultado
+    }
+
+    if(operacao==3){
+        sv = 0
+        n1int = parseInt(pv)
+        restxt = botao.value
+        resint = parseInt(restxt)
+        resultado = n1int * resint
+        pv = resultado
+    }
+
+    if(operacao==4){
+        sv = 0
+        n1int = parseInt(pv)
+        restxt = botao.value
+        resint = parseInt(restxt)
+        resultado = n1int / resint
+        pv = resultado
+    }
     botao.value = ""
     operacao = 3
     
@@ -74,11 +164,41 @@ function divisao(){
         botao.value = ""
         operacao = 4
 } else{
-    sv = botao.value
-    n1int = parseInt(pv)
-    n2int = parseInt(sv)
-    resultado = n1int / n2int
-    pv = resultado
+    if(operacao==1){
+        sv = 0
+        n1int = parseInt(pv)
+        restxt = botao.value
+        resint = parseInt(restxt)
+        resultado = n1int + resint
+        pv = resultado
+    }
+
+    if(operacao==2){
+        sv = 0
+        n1int = parseInt(pv)
+        restxt = botao.value
+        resint = parseInt(restxt)
+        resultado = n1int - resint
+        pv = resultado
+    }
+
+    if(operacao==3){
+        sv = 0
+        n1int = parseInt(pv)
+        restxt = botao.value
+        resint = parseInt(restxt)
+        resultado = n1int * resint
+        pv = resultado
+    }
+
+    if(operacao==4){
+        sv = 0
+        n1int = parseInt(pv)
+        restxt = botao.value
+        resint = parseInt(restxt)
+        resultado = n1int / resint
+        pv = resultado
+    }
     botao.value = ""
     operacao = 4
     
