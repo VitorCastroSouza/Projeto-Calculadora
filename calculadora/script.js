96a17ef8b527a7b60e711be6de4338b937048eae
Vitor de Castro Souza <vitor_c._souza@hotmail.com>
2023-01-28 12:54:52 -0300

    changes

--- calculadora/script.js
+++ calculadora/script.js
@@ -1,11 +1,111 @@
-n1txt = window.document.getElementById('n1')
-n2txt = window.document.getElementById('n2')
-resposta = window.document.getElementById('res')
+let botao = window.document.getElementById('tela')
+let pv = 0
+let sv
+let operacao
+let resultado
+
+function conferir(){
+    if(operacao==1){
+        sv = 0
+        n1int = parseInt(pv)
+        restxt = botao.value
+        resint = parseInt(restxt)
+        resultado = n1int + resint
+        pv = resultado
+    }
+
+    if(operacao==2){
+        sv = 0
+        n1int = parseInt(pv)
+        restxt = botao.value
+        resint = parseInt(restxt)
+        resultado = n1int - resint
+        pv = resultado
+    }
+
+    if(operacao==3){
+        sv = 0
+        n1int = parseInt(pv)
+        restxt = botao.value
+        resint = parseInt(restxt)
+        resultado = n1int * resint
+        pv = resultado
+    }
+
+    if(operacao==4){
+        sv = 0
+        n1int = parseInt(pv)
+        restxt = botao.value
+        resint = parseInt(restxt)
+        resultado = n1int / resint
+        pv = resultado
+    }
+}
+
+function mostrar(digito){
+    botao.value += digito
+}
+
+function limpar(){
+    botao.value = ""
+    resultado = 0
+    pv = 0
+    sv = 0
+}
+
 function soma(){
+
+
+    if(pv==0){
+        pv = botao.value
+        botao.value = ""
+        operacao = 1
+} else{
+    conferir()
+    botao.value = ""
+    operacao = 1
+    }
+}
+
+function subtracao(){
+    if(pv==0){
+        pv = botao.value
+        botao.value = ""
+        operacao = 2
+} else{
+    conferir()
+    botao.value = ""
+    operacao = 2
+    }
+}
+
+function multiplicacao(){
+    if(pv==0){
+        pv = botao.value
+        botao.value = ""
+        operacao = 3
+} else{
+    conferir()
+    botao.value = ""
+    operacao = 3
+    }
+}
+
+function divisao(){
+    if(pv==0){
+        pv = botao.value
+        botao.value = ""
+        operacao = 4
     
-    n1int = parseInt(n1txt.value)
-    n2int = parseInt(n2txt.value)
-    richtroll = n1int + n2int
-    resposta.innerHTML = richtroll
-    
+     } else{ conferir()
+    botao.value = ""
+    operacao = 4
+    }
+}
+
+
+function resul(){
+    conferir()
+    botao.value = resultado
+    operacao = 0
 }
